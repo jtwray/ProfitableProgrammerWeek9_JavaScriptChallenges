8fad8c1c8b6998378a3a5bc2b136626a7cd27d0b
Tucker Wray <tuckerwray84@gmail.com>
2018-11-01 15:21:09 -0400

    Challenge_3 Update> hashed out js functions for backend Rock Paper Scissors

--- JavaScript_week9/static/js/script.js
+++ JavaScript_week9/static/js/script.js
@@ -1,53 +1,67 @@
-//challenge 1 your age in Days
-function AgeinDays(){
-  var birthYear= prompt("What year were you born?");
-  var AgeinDayss= (2018-birthYear) * 365;
-  var h1 = document.createElement('h1');
-  var textAnswer =  document.createTextNode('You are ' + AgeinDayss + 'days old.')
-  h1.setAttribute('id', 'AgeinDays');
+//Challenge 1: Your Age in days
+
+function ageInDays() {
+  var birthYear = prompt('What year were you born...?');
+  var ageInDayss = (2018 - birthYear) * 365;
+  var h1 = doument.createElement('h1');
+  var textAnswer = document.createTextNode('You are ' + ageInDayss + ' days old.');
+  h1.setAttribute('id', 'ageInDays');
   h1.appendChild(textAnswer);
   document.getElementById('flex-box-result').appendChild(h1);
-  console.log(AgeinDayss);
 }
 
-function reset(){
-  document.getElementById('AgeinDays').remove();
+function reset() {
+  document.getElementById('ageInDays').remove();
 }
-//Challenge 2 Generate Cat
-function generateCat(){
+
+//Challenge 2: Cat generator
+function generateCat() {
   var image = document.createElement('img');
-  var div = document.getElementById("flex-cat-generator");
-  image.src ="http://thecatapi.com/api/images/get?format=src&type=gif&size=small";
+  var div = document.getElementById('flex-cat-gen');
+  image.src = "http://thecatapi.com/api/images/get?format=src&type=gif&size=small";
   div.appendChild(image);
 }
-//Challenge 3: Paper Rock Scissors
+
+//Challenge 3: Paper, Rock, Scissors
 function rpsGame(yourChoice) {
   console.log(yourChoice);
   var humanChoice, botChoice;
-  //humanChoice = yourChoice.id;
-  botChoice = randomPrsInt(numberToChoice());
-  //results = decideWinner(humanChoice, botChoice);// humanLost,botWon[0,1]humanWon,botLost[1,0]tieDraw[.5,.5]
-  //message = finalMessage(results); //You Won!|You Lost!|Draw!//{'message':'You Won!', 'color':'Green'}
-  //rpsFrontEnd(yourChoice.id, botChoice, message);
+  humanChoice = yourChoice.id;
+  botChoice = numberToChoice(randToRpsInt());
+  console.log('Computer choice:', botChoice);
+  results = decideWinner(humanChoice, botChoice); //[1,0] human won|bot lost
+  console.log(results);
+  message = finalMessage(results); {'You Won!', 'color': 'green'}
+  //rpsFrontEnd(youChoice.id, botChoice, message);
 }
-function randomPrsInt(){
-  return Math.floor(Math.random() * 3);
 
+function randToRpsInt() {
+  return Math.floor(Math.random() * 3);
 }
-//var botChoices = ["paper","rock","scissors"];
 
 function numberToChoice(number) {
-  return ["paper","rock","scissors"][number];
+  return ['rock', 'paper', 'scissors'][number];
 }
 
-}
-function decideWinner(yourChoice,botChoice){
-  var rpsDatabase= {
-      'paper'{'rock':1,'paper':0.5,'scissors':0};
-      'rock'{'scissors':1,'rock':0.5,'paper':0};
-      'scissors'{'paper':1,'scissors':0.5,'rock':0};
+function decideWinner(yourChoice, computerChoice) {
+  var rpsDatabase = {
+    'rock': {'scissors': 1, 'rock': 0.5, 'paper': 0},
+    'paper':{'rock': 1, 'paper': 0.5, 'scissors': 0},
+    'scissors':{'paper': 1, 'scissors': 0.5, 'rock': 0}
   };
-  var yourScore = rpsDatabase[yourChoice][botChoice];
-  var botScore = rpsDatabase[botChoice][yourChoice];
-  return [yourScore,botScore];
-}   
+
+  var yourScore = rpsDatabase[yourChoice][computerChoice];
+  var computerScore = rpsDatabase[computerChoice][yourChoice];
+
+  return [yourScore, computerScore];
+}
+
+function finalMessage([yourScore, computerScore]) {
+  if (yourScore === 0) {
+    return {'message': 'You lost!', 'color': 'red'};
+  } else if (yourScore === 0.5) {
+    return {'message': 'You tied!', 'color': 'yellow'};
+  } else {
+    return {'message': 'You Won!', 'color': 'green'};
+  }
+}
