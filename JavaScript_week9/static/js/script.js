900321798a1e1ab1f8e509ae289aca67736e5b53
Tucker Wray <tuckerwray84@gmail.com>
2018-11-06 13:09:49 -0500

    Created Backend-Javascript functions for Challenge 5: BlackJack. defined players 'you'&'dealer'. defined var blackjackGame using 'let' to include two parameter you and dealer. each is linked to the scoreSpan tags on html file to allow the updating of score as games conclude. you and dealer are also linked to the your-box and dealer-box divs, respectively.  Created const YOU & DEALER respectively. Created showCard() function to show the card dealt on table and linked soundfile on Hit button press with const hitSound . added folder of card images and linked src in this function.
     implemented use of querySelector in place of getElementById or similar 
    modes.

--- JavaScript_week9/static/js/script.js
+++ JavaScript_week9/static/js/script.js
@@ -178,3 +178,28 @@ function randomColors() {
     all_buttons[i].classList.add(choices[randomNumber]);
   }
 }
+//Challenge 5: BlackJack
+
+let blackjackGame = {
+  'you' : {'scoreSpan': '#your-blackjack-result', 'div': '#your-box', 'score':0},
+  'dealer' : {'scoreSpan': '#dealer-blackjack-result', 'div': '#dealer-box', 'score':0},
+
+};
+
+const YOU = blackjackGame['you'];
+const DEALER = blackjackGame['dealer'];
+
+const hitSound = new Audio('static/sound/swish.m4a');
+
+document.querySelector('#blackjack-hit-button').addEventListener('click', blackjackHit);
+
+function blackjackHit() {
+  showCard(YOU);
+};
+
+function showCard(activePLayer) {
+  let cardImage = document.createElement('img');
+  cardImage.src = 'static/images/Q.png';
+  document.querySelector(activePlayer['div']).appendChild(cardImage);
+  hitSound.play();
+};
